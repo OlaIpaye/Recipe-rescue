7d9744cd396f80b4935496628de0617f7fca775b
Vasile <vasileholding@gmail.com>
2023-08-24 10:54:48 +0100

    declared const

---
+++ assets/js/secondScript.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  const searchButton = $("#searchBarButton");
+  const appId = "39493a96";
+  const apiKey = "5ab9dc34c074ab5086a207e32c79a563";
+  const headerSection = $(".container-fluid.row.d-flex.justify-content-center");
+  const headerButtons = $(".headerButton");
+});
