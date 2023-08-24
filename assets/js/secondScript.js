7765b0abbcb525638cd68f6eaa396a8b18751001
Vasile <vasileholding@gmail.com>
2023-08-24 10:59:02 +0100

    added event listener for displaying and hiding content on the page when clicked

--- assets/js/secondScript.js
+++ assets/js/secondScript.js
@@ -1,7 +1,18 @@
 $(document).ready(function () {
+  // ----------Declared Variables--------
   const searchButton = $("#searchBarButton");
   const appId = "39493a96";
   const apiKey = "5ab9dc34c074ab5086a207e32c79a563";
   const headerSection = $(".container-fluid.row.d-flex.justify-content-center");
   const headerButtons = $(".headerButton");
+
+  //   ------------hide content and display it function-------
+  headerButtons.on("click", function () {
+    const target = $(this).data("target");
+    const selectedValue = $(this).data("value");
+    headerSection.addClass("d-none");
+    $("#" + target).removeClass("d-none");
+    // searchQuery = $(this).data("value");
+    fetchAPI(selectedValue);
+  });
 });
