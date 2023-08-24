8db7522dad6ebb4416c5ab74f13e1ce0183c54b2
Rinkal <email@example.com>
2023-08-24 12:33:36 +0100

    added const varible for serch fetch

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    const resultsDiv = $("#results");
+    const cocktailsDiv = $("#cocktails"); 
+    const recipeModal = $("#recipeModal");
+    const cocktailModal = $("#cocktailModal")
+    const modalTitle = $("#modalTitle");
+    const cuisineT = $("#cuisineType");
+    const totalT = $("#totalTime");
+    const ingredientL = $("#ingredientLines");
+    const totalNuts = $("#totalNutrients");
+    const sourceid = $("#source");
+    const imageid = $("#image");
+
+    const comodalTitle = $("#comodalTitle");
+    const cocktailid = $("#cocktailid");
+    const instruct = $("#instructions");
+    const ingr1 = $("#ingredient1") ;
+    const ingr2 = $("#ingredient2");
+    const ingr3 = $("#ingredient3");
+    const ingr4 = $("#ingredient4");
+    const alco = $("#alcoholic") 
+    const gla = $("#glass");
+
+    const modalBody = $("#modalBody");
+    const modalBackdrop = $("#modalBackdrop"); 
+    const nextButton = $("#nextButton");
+    const prevButton = $("#prevButton");
+    
+});
